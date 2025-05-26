7f2c0473525ebe05095fb4ee8d80f928f432c33f
Aaditya Ghogale <aadityaghogale01@gmail.com>
2025-05-26 20:12:45 +0530

    arithemetic operations

---
+++ Learn Java/ops.java
@@ -0,0 +1,16 @@
+class ops{
+    public static void main(String args[]) {
+        operations();
+        System.out.println("Hello World");
+    }
+
+    public static void operations(){
+        int a=10,b=5;
+        int sum=a+b;
+        int diff=a-b;
+        int prod=a*b;   
+        int quot=a/b;
+        int rem=a%b;
+        System.out.println(sum +" "+diff+" "+prod+" "+quot+" "+rem); 
+    }
+}
