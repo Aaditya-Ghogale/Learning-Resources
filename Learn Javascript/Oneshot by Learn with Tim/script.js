cec543e84a8d9fc148894d1223cc8b27f619aa79
Aaditya Ghogale <aadityaghogale01@gmail.com>
2026-01-06 01:52:25 +0530

    Update Learn Javascript/Oneshot by Learn with Tim/script.js

--- Learn Javascript/Oneshot by Learn with Tim/script.js
+++
@@ -1,7 +0,0 @@
-const yoe =6
-
-console.log("i am in script.js now")
-console.error("I feel that CS is getting harder now that i am. ",21," years old")
-console.log(`When I have ${yoe} y.o.e. i expect myself to be more confident, more wealthier, and earning the big bucks also post grad from a top uni in india or ms from a decent uni abroad`)
-console.warn("What is ai gonna do?")
-// alert("Lets try we dont have any way out any way.")
