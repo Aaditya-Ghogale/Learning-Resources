ab59d9a3f027f0920387945f184e458e0965fa5a
Aaditya Ghogale <aadityaghogale01@gmail.com>
2026-01-14 23:52:45 +0530

    Update Learn Javascript/JavaScript Speedrun for Programmer/test.js

--- Learn Javascript/JavaScript Speedrun for Programmer/test.js
+++ Learn Javascript/JavaScript Speedrun for Programmer/test.js
@@ -1,12 +1,2 @@
-let a=[1,2,3,4,5];
-// a.push(6,7,8,9,"hi", [12,34,5,566,778]);
-// console.log(a[10][3]);
-// console.log(a);
-// a.shift();
-// console.log(a);
-// // a.pop();
-// // console.log(a);
-
-a.unshift({id:1, name:"Ramesh"});
-console.log(a);
-console.log(a[0].id);
+let a=[1,2,3,4,5,6,7];
+console.log(a.splice(2,2));
